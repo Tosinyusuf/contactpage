86cdac5acf7358532e4c072cdbadd6f77dd4e31a
Yusuf Tosin Azeez <yusuftosco66@gmail.com>
2020-11-19 01:43:29 +0100

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,8 @@
+// function myFunction() {
+//     var y = document.getElementById("nav-list")
+//     if (y.style.display === "none") {
+//         y.style.display = "block";
+//     } else {
+//         y.style.display ="none";
+//     }
+// }
